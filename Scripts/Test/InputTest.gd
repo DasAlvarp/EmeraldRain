fb36570f5d6862f9ae5923d9ce5b92e4724990df
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2017-10-02 15:26:45 -0500

    Even better structure. Still got 20 minutes to make it perfect.

--- Scripts/Test/InputTest.gd
+++ Scripts/Test/InputTest.gd
@@ -1,27 +1,22 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var InputReader = load("./InputReader.gd")
+var InputReader = load("res://Scripts/Implemented/Universal/InputReader.gd")
 
 var reader
 var reader2
 
 
 func _ready():
+	#how button maps are registered. In an actual game, this would be set semi-dynamically at the start.
 	var aButtons = [2]
 	var bButtons = [0, 5]
 	var cButtons = [3, 5]
 	var dButtons = [1]
 	reader = InputReader.InputReader.new(0, aButtons, bButtons, cButtons, dButtons, 15, false)
 	reader2 = InputReader.InputReader.new(1, aButtons, bButtons, cButtons, dButtons, 15, true)
-	# Called every time the node is added to the scene.
-	# Initialization here
-
 	set_process(true)
-	pass
-	
+
+
 func _process(delta):
 	reader.updateBuffer()
 	reader2.updateBuffer()
