96215d2aa8b94058ff0736e331d59ef612aeba9e
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2017-10-05 13:28:41 -0500

    This does not run right now. Look at this good practice with branches!

--- Scripts/Implemented/Universal/InputTranslator.gd
+++ Scripts/Implemented/Universal/InputTranslator.gd
@@ -22,11 +22,78 @@ class InputTranslator:
 
 
 	func getMove(row):
-		var gesture = moveList.getEntry("direction", row)
-		var stickiness = gesture[-1]
+		var gesture = moveList.getEntry(row, "direction")
 		#for reading buttons:
 		#if a button is pressed, check for 2s.
 		#if the button state doesn't matter, check for 0s
 		#if the button needs to be down, check for 1s.
 		#if the button needs to be up check for -1s.
 		#if the button needs to be released, check for -2s.
+		#buffer[0] = aPressedBuffer
+		#buffer[1] = bPressedBuffer
+		#buffer[2] = cPressedBuffer
+		#buffer[3] = dPressedBuffer
+		#buffer[4] = getDirectionalBuffer()
+		
+		#first make sure inputs are fine. Only have to worry about f1, so yeah.
+		var inputs = inputReader.getReadBuffer()
+		var a = moveList.getEntry("a", row)
+		var b = moveList.getEntry("b", row)
+		var c = moveList.getEntry("c", row)
+		var d = moveList.getEntry("d", row)
+		
+		#for now, we'll just care about button pressed
+		if(getButtonInput(a, str(inputs[0][-1])) && getButtonInput(b, str(inputs[1][-1])) && getButtonInput(c, str(inputs[2][-1])) && getButtonInput(d, str(inputs[3][-1]))):
+			if(matchGesture(gesture, inputs)):
+				return moveList.getEntry("state", row)
+			else:
+				return -1
+		else:
+			return -1
+
+
+	func matchGesture(gesture, inputs):
+		var stickiness = gesture.right(gesture.length() - 1)
+		var gestureContents = getTop(gesture.substr(0, gesture.length() - 1))
+		var gestureKillers = gestureContents[1]
+		var topIndex = gestureContents[0]
+		gesture = gestureContents[2]
+		for direction in inputs[4]:
+			if(gestureKillers.has(str(direction))):
+				return false
+			elif(topIndex == str(direction)):
+				gestureContents = getTop(gesture.substr(0, gesture.length()))
+				gestureKillers = gestureContents[1]
+				topIndex = gestureContents[0]
+				gesture = gestureContents[2]
+				if(gesture.length() == 0):
+					return true
+		
+		return false
+
+
+	func getTop(gesture):
+		var gestureKillers = []
+		var inBrackets = false
+		for negIndex in range(gesture.length() - 1):
+			var topChar = gesture.substr(gesture.length() - (1 + negIndex), 1)
+			if(topChar == "]"):
+				inBrackets = true
+			elif(topChar == "["):
+				inBrackets = false
+			elif(inBrackets):
+				gestureKillers.append(topChar)
+			else:
+				var returnMe = []
+				returnMe[0] = topChar
+				returnMe[1] = gestureKillers
+				returnMe[2] = gesture.substr(0, gesture.length() - negIndex)
+
+	func getButtonInput(buttonLetter, realInput):
+		if(buttonLetter != "0"):
+			if(buttonLetter == str(realInput)):
+				return true
+			else:
+				return false
+		else:
+			return true
--- Scripts/Test/InputTest.gd
+++ Scripts/Test/InputTest.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var InputReader = load("res://Scripts/Implemented/Universal/InputReader.gd")
 var CSVreader = load("res://Scripts/Implemented/Universal/CSVreader.gd")
-
+var InputTranslator = load("res://Scripts/Implemented/Universal/InputTranslator.gd")
 var reader
 var reader2
+var converter
 var csv
+var state
 
 
 func _ready():
@@ -15,9 +17,10 @@ func _ready():
 	var cButtons = [3, 5]
 	var dButtons = [1]
 	#initialize controllers. They will do things
-	csv = CSVreader.CSVreader.new("res://Assets/Test/TestCols.csv")
+	csv = CSVreader.CSVreader.new("res://Assets/Test/GestureDemo.csv")
 	reader = InputReader.InputReader.new(0, aButtons, bButtons, cButtons, dButtons, 15, false)
 	reader2 = InputReader.InputReader.new(1, aButtons, bButtons, cButtons, dButtons, 15, true)
+	converter = InputTranslator.InputTranslator.new(csv, reader)
 	set_process(true)
 
 
@@ -25,7 +28,10 @@ func _process(delta):
 	#check controllers
 	reader.updateBuffer()
 	reader2.updateBuffer()
+	state = converter.getGesture()
+	if(state != -1):
+		get_node("Control/CSV").set_text(state)
 	#display output
-	get_node("Control/CSV").set_text(csv.getEntry("Fweh", 7))
+
 	get_node("Control/P1Input").set_text(reader.getDisplayBuffer())
 	get_node("Control/P2Input").set_text(reader2.getDisplayBuffer())
