6326e0e544ce578fcf2ce1a58240405d5a72c0a2
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2017-10-11 14:39:19 -0500

    Gonna implemen5t stuff

--- Scripts/Implemented/Character/Em/Controller.gd
+++ Scripts/Implemented/Character/Em/Controller.gd
@@ -1,6 +1,23 @@
 class Controller:
 	var inputReader
 	var skin
+
+
 	func _init(skin, inputReader):
 		self.skin = skin
 		self.inputReader = inputReader
+	
+	func getMaxHp():
+		return 1000
+
+
+	func getMaxMeter():
+		return 3
+
+
+	func getStartingRes():
+		return 1
+
+
+	func getStartingFlow():
+		return 1
