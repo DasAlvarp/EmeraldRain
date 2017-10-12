67906db92ef1dc5ea4c6090732ce997dafd8066b
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2017-10-12 08:21:57 -0500

    Getstate has been set up. Let's make the cancel tables IRL and get this going.

--- Scripts/Implemented/Character/Em/Controller.gd
+++ Scripts/Implemented/Character/Em/Controller.gd
@@ -2,26 +2,32 @@ class Controller:
 	var inputReader
 	var skin
 
+	var stateTable
+	var inputTable#will be array of N tables of all the cancel states. Every cahracter will have at least 14: 4 for normals (a->a+, b->c+. c->d+, d->sp+, sp->super, dashing forward, standing, and all of this shit for air. That's assuming the state is cancellable.
+	var inputTranslator
 
 	func _init(skin, inputReader):
 		self.skin = skin
 		self.inputReader = inputReader
-	
+		self.inputTranslator = load("res://Scripts/Implemented/Universal/InputTranslator.gd").InputTranslator.new(inputTable[0], inputReader)
+
+
+	#not following my formatting form b/c these functions are too basic
 	func getMaxHp():
 		return 1000
-
 
 	func getMaxMeter():
 		return 3
 
-
 	func getStartingRes():
 		return 1
-
 
 	func getStartingFlow():
 		return 1
 
-
 	func getMaxWeight():
 		return 400
+
+
+	func getState(state, meter, resources, grounded):
+		
