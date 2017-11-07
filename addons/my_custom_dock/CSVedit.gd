c294801b97dcabc187893478b5577e84dabd9460
DESKTOP-Q7EPA2R\a'“ <aag781@utulsa.edu>
2017-11-07 08:48:29 -0600

    Button stuff.

--- addons/my_custom_dock/CSVedit.gd
+++ addons/my_custom_dock/CSVedit.gd
@@ -7,7 +7,7 @@ func _enter_tree():
 	dock = preload("res://addons/my_custom_dock/my_dock.tscn").instance()
 	# Called every time the node is added to the scene.
 	# Initialization here
-	add_control_to_dock( DOCK_SLOT_LEFT_UL, dock)
+	add_control_to_dock( DOCK_SLOT_LEFT_BL, dock)
 
 func _exit_tree():
 	remove_control_from_docks(dock)
